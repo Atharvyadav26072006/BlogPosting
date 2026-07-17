df7d92538b6aee2b6fca2a1ffeee730eeec71b64
ATHARV YADAV <atharvy728@gmail.com>
2026-07-17 18:20:26 +0530

    Add default image

--- obj/Debug/net10.0/SyncSyntax.AssemblyInfo.cs
+++ obj/Debug/net10.0/SyncSyntax.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c201c61bf91472263a8b87a69a4d27b897578f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25e79c56ef22d3b6c3e884381d9ecef5739b66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyncSyntax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
